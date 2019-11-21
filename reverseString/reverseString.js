71387c7b921c6d557d81e37e515c639dd53fc570
codingyogurt <28801036+codingyogurt@users.noreply.github.com>
2019-11-21 14:28:45 +0800

    Finished reverseString challenge

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strings = string.split("");
+    let stringReversed = "";
+    for(i = strings.length - 1; i >= 0; i--){
+        stringReversed += strings[i];
+    }
+    return stringReversed;
 }
 
 module.exports = reverseString

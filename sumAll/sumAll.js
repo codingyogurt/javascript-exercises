aee878f4450da7457adf9163fc646bc2a39cb8fe
codingyogurt <28801036+codingyogurt@users.noreply.github.com>
2019-11-21 21:57:24 +0800

    finished sumAll challenge

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
 
+    if (typeof startNum !== "number" ||
+        typeof endNum !== "number" ||
+        startNum < 0 ||
+        endNum < 0 ) return "ERROR";
+
+    if (startNum > endNum){
+        const temp = startNum;
+        startNum = endNum;
+        endNum = temp;
+    }
+
+    let sum = 0;
+    for(i = startNum; i <= endNum; i++){
+        sum += i;
+    }
+    return sum;
 }
 
 module.exports = sumAll

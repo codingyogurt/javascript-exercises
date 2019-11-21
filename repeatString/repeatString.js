c44a2379a296078707419113590dd0321016b188
codingyogurt <28801036+codingyogurt@users.noreply.github.com>
2019-11-21 14:13:28 +0800

    done with repeatString challenge

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let allText = "";
+    if (num >= 0){ 
+        for(i = 0; i < num; i++){
+            allText += text;
+        }
+    } else {
+        return "ERROR";
+    }
+    return allText;
 }
 
 module.exports = repeatString

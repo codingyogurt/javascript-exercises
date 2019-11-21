0ee324991ff4abfec482442fed71baf2e2b9e32f
codingyogurt <28801036+codingyogurt@users.noreply.github.com>
2019-11-21 22:30:25 +0800

    finished tempConversion challenge

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  result = (fahrenheit-32) * (5/9);
+  return round1Dec(result);
 }
 
-const ctof = function() {
+const ctof = function(celsius) {
+  result = celsius * (9/5) + 32;
+  return round1Dec(result);
+}
 
+function round1Dec(num){
+  return Math.round(num * 10) / 10; 
 }
 
 module.exports = {

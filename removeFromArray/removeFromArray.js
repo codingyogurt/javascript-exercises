71f8eee1cf1091ee3c437351d55d2ca7afc6927f
codingyogurt <28801036+codingyogurt@users.noreply.github.com>
2019-11-21 15:17:47 +0800

    Finished remove from array challenge

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, remove) {
+    for (i = 1; i < arguments.length; i++){
+        let index = arr.indexOf(arguments[i]);
+        if (index != -1){
+            arr.splice(index,1);
+        }
+    }
+    return arr;
 }
 
 module.exports = removeFromArray
